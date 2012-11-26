3dc9b7044f1320d7bc5ff5f098fc6de07e229a2a
Karsten <kjensmo@gmail.com>
2012-11-26 19:21:06 +0100

    changed "later" to "another time"

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void HelloW(){
 		
 	String hello;
-	hello = "Bye World, see you later!";
+	hello = "Bye World, see you another time!";
 	
 	System.out.println(hello);
 	}
