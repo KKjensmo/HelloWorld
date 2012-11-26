1caa2f84cf9313aba224eefbdc78c0276bcad88b
Karsten <Karsten@Skynet.lan>
2012-11-26 19:29:42 +0100

    changed to 'see you at school tomorrow'

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void HelloW(){
 		
 	String hello;
-	hello = "Bye World, see you another time!";
+	hello = "Bye World, see you at school tomorrow.";
 	
 	System.out.println(hello);
 	}
